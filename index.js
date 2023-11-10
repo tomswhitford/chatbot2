807411318aab60ce8e6488487ccd3bd0539b4d6b
tomswhitford <150309968+tomswhitford@users.noreply.github.com>
2023-11-10 10:26:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require ('cors');
 require('dotenv').config();
 
 const webApp = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 webApp.use (cors ());
 
